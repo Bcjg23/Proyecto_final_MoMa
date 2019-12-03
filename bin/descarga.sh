b17b9beeabea602a4580ab69387658b1a5d6f2c0
”sonialr” <sososoniararara@gmail.com>
2019-12-02 21:12:53 -0600

    Codigo par bajas los archivos csv

---
+++ bin/descarga.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+curl -o data/Artists.csv https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artists.csv
+
+curl -o data/Artists.json https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artists.json
+
+curl -o data/Artworks.csv https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artworks.csv
+
+curl -o data/Artworks.json https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artworks.json
